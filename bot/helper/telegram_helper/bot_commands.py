2273f8790315c33daa62d7164d6f8f6adc3bec06
SuggalaVamsi <88180621+SuggalaVamsi@users.noreply.github.com>
2021-11-03 04:52:11 -0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,10 +1,9 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.ListCommand = 'list'
+        self.ListCommand = 'search'
         self.AuthorizedUsersCommand = 'users'
         self.AuthorizeCommand = 'auth'
         self.UnAuthorizeCommand = 'unauth'
-        self.LogCommand = 'log'
 
 BotCommands = _BotCommands()
